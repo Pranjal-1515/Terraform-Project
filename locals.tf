ee6ce71cad525d128458af9c25daf186bf213f67
Pranjal <33702599+Pranjal-1515@users.noreply.github.com>
2025-05-26 13:57:05 +0530

    Create locals.tf
    Adding locals file

---
+++ locals.tf
@@ -0,0 +1,21 @@
+locals {
+  ingress_rules = [
+    {
+      description = "Allow SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress_rules = [
+    {
+      description = "Allow all outbound traffic"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
