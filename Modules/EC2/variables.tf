c0beb4b9cdcd2616619ae217a77ba5442fa01ace
Pranjal <33702599+Pranjal-1515@users.noreply.github.com>
2025-05-26 09:18:47 +0530

    Create variables.tf

---
+++ Modules/EC2/variables.tf
@@ -0,0 +1,22 @@
+# Declaring variables for ec2 module
+
+variable "instance_count"{
+    description = "Number of EC2 to create"
+    type = number
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the instance"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix for the instance names"
+  type        = string
+}
