80f66759bab2cd2d86cd02b18428e8066e208111
Pranjal <33702599+Pranjal-1515@users.noreply.github.com>
2025-05-29 12:53:07 +0530

    Update variables.tf
    Adding variables

--- variables.tf
+++ variables.tf
@@ -45,3 +45,19 @@ variable "tags" {
   description = "Tags for the S3 bucket"
   default     = {}
 }
+
+variable "group_name" {
+  description = "The name of the IAM group"
+  type        = string
+}
+
+variable "user_name" {
+  description = "The name of the IAM user"
+  type        = string
+}
+
+variable "policy_arn" {
+  description = "The ARN of the IAM policy to attach to the group"
+  type        = string
+}
+
